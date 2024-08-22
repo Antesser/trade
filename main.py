b876df769dffc30bf19470e2e34e7ff4d4cfec41
Antesser <ahriman.kun@gmail.com>
2024-08-22 17:18:51 +0300

    added auth

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from typing import Any, Dict, List
-from fastapi import FastAPI, Request, status
+
+from fastapi import Depends, FastAPI, Request, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import ResponseValidationError
 from fastapi.responses import JSONResponse
+from fastapi_users import FastAPIUsers
 
-
+from auth.auth import auth_backend
+from auth.manager import get_user_manager
+from auth.schemas import UserCreate, UserRead
 from models.schemas import Trade, User
-
 
 app = FastAPI(title="App for trade")
 
@@ -64,3 +67,33 @@ async def get_user(user_id: int) -> List[Dict[str, Any]]:
 async def add_trades(trades: List[Trade]):
     fake_trades.extend(trades)
     return {"status": 200, "data": fake_trades}
+
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+
+current_user = fastapi_users.current_user()
+
+
+@app.get("/protected-route")
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.username}"
+
+
+@app.get("/unprotected-route")
+def unprotected_route():
+    return "Hello, man"
--- models/models.py
+++ models/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timezone
 
 from sqlalchemy import (
+    Boolean,
     MetaData,
     Integer,
     String,
@@ -14,21 +15,28 @@ from sqlalchemy import (
 
 metadata = MetaData()
 
-roles = Table(
-    "roles",
+role = Table(
+    "role",
     metadata,
     Column("id", Integer, primary_key=True),
     Column("name", String, nullable=False),
     Column("permissions", JSON),
 )
 
-users = Table(
-    "users",
+user = Table(
+    "user",
     metadata,
     Column("id", Integer, primary_key=True),
     Column("email", String, nullable=False),
     Column("username", String, nullable=False),
-    Column("password", String, nullable=False),
-    Column("registered_at", TIMESTAMP, default=datetime.now(timezone.utc)),
-    Column("role_id", Integer, ForeignKey("roles.id")),
+    Column("hashed_password", String, nullable=False),
+    Column(
+        "registered_at",
+        TIMESTAMP(timezone=True),
+        default=datetime.now(timezone.utc),
+    ),
+    Column("role_id", Integer, ForeignKey(role.c.id)),
+    Column("is_active", Boolean, default=True, nullable=False),
+    Column("is_superuser", Boolean, default=False, nullable=False),
+    Column("is_verified", Boolean, default=False, nullable=False),
 )
