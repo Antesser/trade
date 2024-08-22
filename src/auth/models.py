93306490240c1ba8d30e2c9844e07f2a1cb3d3ef
Antesser <ahriman.kun@gmail.com>
2024-08-22 19:29:30 +0300

    Revert "added auth" This reverts commit b876df769dffc30bf19470e2e34e7ff4d4cfec41.

--- src/auth/models.py
+++ src/auth/models.py
@@ -2,12 +2,6 @@ from datetime import datetime, timezone
 
 from fastapi_users.db import SQLAlchemyBaseUserTable
 from sqlalchemy import (
-    JSON,
-    TIMESTAMP,
-    Boolean,
-    Column,
-    ForeignKey,
-    Integer,
     MetaData,
     String,
     Table,
@@ -16,41 +10,21 @@ from sqlalchemy.orm import DeclarativeBase
 
 metadata = MetaData()
 
-role = Table(
-    "role",
+roles = Table(
+    "roles",
     metadata,
     Column("id", Integer, primary_key=True),
     Column("name", String, nullable=False),
     Column("permissions", JSON),
 )
 
-user = Table(
-    "user",
+users = Table(
+    "users",
     metadata,
     Column("id", Integer, primary_key=True),
     Column("email", String, nullable=False),
     Column("username", String, nullable=False),
-    Column("hashed_password", String, nullable=False),
-    Column(
-        "registered_at",
-        TIMESTAMP(timezone=True),
-        default=datetime.now(timezone.utc),
-    ),
-    Column("role_id", Integer, ForeignKey(role.c.id)),
-    Column("is_active", Boolean, default=True, nullable=False),
-    Column("is_superuser", Boolean, default=False, nullable=False),
-    Column("is_verified", Boolean, default=False, nullable=False),
+    Column("password", String, nullable=False),
+    Column("registered_at", TIMESTAMP, default=datetime.now(timezone.utc)),
+    Column("role_id", Integer, ForeignKey("roles.id")),
 )
-
-
-class Base(DeclarativeBase):
-    pass
-
-
-class User(SQLAlchemyBaseUserTable[int], Base):
-    id = Column(Integer, primary_key=True)
-    username = Column(String, nullable=False)
-    registered_at = Column(
-        TIMESTAMP(timezone=True), default=datetime.now(timezone.utc)
-    )
-    role_id = Column(Integer, ForeignKey(role.c.id))
