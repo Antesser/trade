80ea803a3c69f9972014b2d34a3eca440bb78434
Antesser <ahriman.kun@gmail.com>
2024-08-25 20:24:01 +0300

    Added jinja + templates

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from redis import asyncio as aioredis
@@ -11,6 +13,7 @@ from auth.base_config import auth_backend, current_user, fastapi_users
 from auth.models import User
 from auth.schemas import UserCreate, UserRead
 from operations.router import router as router_ops
+from pages.router import router as router_pages
 from tasks.router import router as router_tasks
 
 
@@ -23,6 +26,7 @@ async def lifespan(_: FastAPI) -> AsyncIterator[None]:
 
 app = FastAPI(title="App for trade", lifespan=lifespan)
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
@@ -48,6 +52,20 @@ def unprotected_route():
 
 app.include_router(router_ops)
 app.include_router(router_tasks)
+app.include_router(router_pages)
+
+
+origins = [
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "PATCH", "UPDATE", "DELETE"],
+    allow_headers=["*"],
+)
 
 
 if __name__ == "__main__":
